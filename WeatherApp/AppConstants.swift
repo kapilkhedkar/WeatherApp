f2e67432b6bf3c4e0286606d2b96041f18442c7b
Kapil Khedkar <31789869+kapilkhedkar@users.noreply.github.com>
2022-09-10 10:02:33 +0530

    First upload

---
+++ WeatherApp/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  WeatherApp
+//
+//  Created by Kapil Khedkar on 10/09/22.
+//
+
+import Foundation
+class AppConstants
+{
+    static let apiBaseUrl = "http://api.weatherstack.com/"
+    static let apiAccessKey = "fef7cc19fde848cc7bf22a3158a07504"
+    static let getCurrentWeatherAPI = apiBaseUrl+"current?access_key="+apiAccessKey+"&query="
+    
+}
